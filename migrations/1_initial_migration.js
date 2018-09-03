2335f3da673e88be97b21d4bf5c44b8b3d6881ff
Hieu <hieudoan2609@gmail.com>
2018-09-03 18:14:09 +0700

    update truffle migration

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -11,10 +11,12 @@ const unitsOneEthCanBuy = process.env.TOKEN_RATE;
 const totalSupply = process.env.TOKEN_SUPPLY;
 
 module.exports = function(deployer) {
-	deployer.deploy(Migrations);
-	deployer.deploy(Token, name, symbol, unitsOneEthCanBuy, totalSupply);
-	deployer.deploy(Exchange);
-	deployer.deploy(RBT);
+	deployer.deploy(Migrations, { overwrite: false });
+	deployer.deploy(Token, name, symbol, unitsOneEthCanBuy, totalSupply, {
+		overwrite: false
+	});
+	deployer.deploy(Exchange, { overwrite: false });
+	deployer.deploy(RBT, { overwrite: false });
 	deployer.link(RBT, ExchangePure);
-	deployer.deploy(ExchangePure);
+	deployer.deploy(ExchangePure, { overwrite: false });
 };
