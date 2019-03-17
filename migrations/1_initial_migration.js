2929147863767f6ae48fd9b2f02702aa58003498
Hieu <hieudoan2609@gmail.com>
2019-03-18 02:02:11 +0700

    add Token.sol

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,16 +1,17 @@
 require("dotenv").config();
 
 const Migrations = artifacts.require("./Migrations.sol");
+const Crowdsale = artifacts.require("./Crowdsale.sol");
 const Token = artifacts.require("./Token.sol");
 const Exchange = artifacts.require("./Exchange.sol");
 const ExchangePure = artifacts.require("./ExchangePure.sol");
 const RBT = artifacts.require("./lib/RedBlackTree.sol");
 
 module.exports = function(deployer) {
-	deployer.deploy(Migrations);
-	deployer.deploy(Exchange);
-	// deployer.deploy(Token);
-	// deployer.deploy(RBT);
-	// deployer.link(RBT, ExchangePure);
-	// deployer.deploy(ExchangePure);
+  deployer.deploy(Migrations);
+  deployer.deploy(Exchange);
+  // deployer.deploy(Token, "1000000000000000000000");
+  // deployer.deploy(RBT);
+  // deployer.link(RBT, ExchangePure);
+  // deployer.deploy(ExchangePure);
 };
