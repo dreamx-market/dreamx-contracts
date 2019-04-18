dbfbdc49ba6d7d4341a53bafbe8b66e8f0fa23e4
Hieu <hieudoan2609@gmail.com>
2019-04-18 14:57:55 +0700

    add .env.sample

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const Migrations = artifacts.require("./Migrations.sol");
+// const Migrations = artifacts.require("./Migrations.sol");
 const Crowdsale = artifacts.require("./Crowdsale.sol");
 const Token = artifacts.require("./Token.sol");
 const Exchange = artifacts.require("./Exchange.sol");
@@ -8,7 +8,7 @@ const ExchangePure = artifacts.require("./ExchangePure.sol");
 const RBT = artifacts.require("./lib/RedBlackTree.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  // deployer.deploy(Migrations);
   deployer.deploy(Exchange);
   deployer.deploy(Token, "1000000000000000000000");
   deployer.deploy(Token, "1000000000000000000000");
