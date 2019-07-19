1abb9b4825de456c74e85afcca99fd029a1b1d7a
Hieu <hieudoan2609@gmail.com>
2019-07-19 15:42:01 +0700

    add tests for conversion logic

---
+++ contracts/Converter.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.22;
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+contract Converter {
+  using SafeMath for uint;
+  
+  constructor() {
+  }
+
+  function calculateTakeAmount(uint _giveAmount, uint _totalGive, uint _totalTake) public pure returns (uint) {
+    uint takeAmount = _totalTake.mul(_giveAmount).div(_totalGive);
+    return takeAmount;
+  }
+
+  function calculateGiveAmount(uint _takeAmount, uint _totalGive, uint _totalTake) public pure returns (uint) {
+    uint giveAmount = _totalGive.mul(_takeAmount).div(_totalTake);
+    return giveAmount;
+  }
+}
